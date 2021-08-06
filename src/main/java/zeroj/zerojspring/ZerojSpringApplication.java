ec0f9e7f70f7acdda16eb0780f9d112b965af7e7
syj <syj990930@naver.com>
2021-08-06 12:52:59 +0900

    Spring Study

---
+++ src/main/java/zeroj/zerojspring/ZerojSpringApplication.java
@@ -0,0 +1,13 @@
+package zeroj.zerojspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerojSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerojSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zeroj/zerojspring/ZerojSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package zeroj.zerojspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerojSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
