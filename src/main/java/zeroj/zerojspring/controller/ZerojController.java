3be72980f638b49ea98dfbb2e35f547cc177ccc8
syj <syj990930@naver.com>
2021-08-07 16:43:35 +0900

    나는야 스프링 공부중

---
+++ src/main/java/zeroj/zerojspring/controller/ZerojController.java
@@ -0,0 +1,51 @@
+package zeroj.zerojspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ZerojController {
+
+//    @GetMapping("hello")
+//    public String hello(Model model){
+//        model.addAttribute("data","hello!!");
+//        return "hello";
+//        //스프링 부트가 resouces안에 templates의 return값 찾음
+//    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+}
